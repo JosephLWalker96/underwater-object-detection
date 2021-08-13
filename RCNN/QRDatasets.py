b74f8e8e646632ec49de1eb6b9b7c24153ea342c
Leo <leozz1999@163.com>
2021-08-13 14:03:10 +0800

    gray scale

--- RCNN/QRDatasets.py
+++ RCNN/QRDatasets.py
@@ -28,7 +28,7 @@ class QRDatasets(Dataset):
 
         img = np.array(cv2.imread(img_path)).astype(np.float32)
         # # converting to grayscale
-        img = np.array(cv2.cvtColor(cv2.imread(img_path)), cv2.COLOR_BGR2GRAY).astype(np.float32)
+        img = np.array(cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2GRAY)).astype(np.float32)
         img /= 255.0
 
         boxes = records[['x', 'y', 'w', 'h']].values 
