4b361bb927070f00bbebddf7dda9b2c116ddabc9
Zheng Zeng <leozz1999@163.com>
2021-11-02 23:17:05 +0800

    Testing

--- RCNN/model.py
+++ RCNN/model.py
@@ -13,7 +13,7 @@ class net(nn.Module):
         super(net, self).__init__()
         self.nn_type = nn_type
         if self.nn_type == "faster-rcnn":
-            self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True, num_classes=num_classes)
+            self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
             # handling gray scale format
 #             if use_grayscale:
@@ -23,7 +23,7 @@ class net(nn.Module):
             in_features = self.model.roi_heads.box_predictor.cls_score.in_features
 
             # replace the pre-trained head with a new one
-            # self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+            self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
         elif self.nn_type == "retinanet":
             self.model = torchvision.models.detection.retinanet_resnet50_fpn(pretrained_backbone=True, num_classes=num_classes)
 #             print(self.model)
