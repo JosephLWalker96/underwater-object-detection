8840ec66db5ebe388894b567b9e8f3f7d0b8f9ea
LeoZZZ <leozz1999@163.com>
2022-04-15 00:05:39 +0800

    Adding vgg16 support (need debug)

--- RCNN/model.py
+++ RCNN/model.py
@@ -8,8 +8,10 @@ from wbf_ensemble import run_wbf, make_ensemble_predictions
 '''
     The baseline model is based on fasterrcnn with a resnet50 as cnn
 '''
+
+
 class net(nn.Module):
-    def __init__(self, num_classes, nn_type = "faster-rcnn", use_grayscale = False):
+    def __init__(self, num_classes, nn_type="faster-rcnn", use_grayscale=False):
         super(net, self).__init__()
         self.nn_type = nn_type
         if self.nn_type == "faster-rcnn":
@@ -21,8 +23,41 @@ class net(nn.Module):
             # replace the pre-trained head with a new one
             self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
         elif self.nn_type == "retinanet":
-            self.model = torchvision.models.detection.retinanet_resnet50_fpn(pretrained_backbone=True, num_classes=num_classes)
-#             print(self.model)
+            self.model = torchvision.models.detection.retinanet_resnet50_fpn(pretrained_backbone=True,
+                                                                             num_classes=num_classes)
+        #             print(self.model)
+        #TODO: need to debug for this one
+        elif self.nn_type == 'faster-rcnn-vgg16':
+            # cite: https://github.com/pytorch/vision/issues/1116#issuecomment-515373212
+            vgg = torchvision.models.vgg16(pretrained=True)
+            backbone = vgg.features[:-1]
+            for layer in backbone[:10]:
+                for p in layer.parameters():
+                    p.requires_grad = False
+            backbone.out_channels = 512
+            anchor_generator = torchvision.models.detection.faster_rcnn.AnchorGenerator(
+                sizes=((32, 64, 128, 256, 512),),
+                aspect_ratios=((0.5, 1.0, 2.0),))
+            roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],
+                                                            output_size=7,
+                                                            sampling_ratio=2)
+            class BoxHead(nn.Module):
+                def __init__(self, vgg):
+                    super(BoxHead, self).__init__()
+                    self.classifier = nn.Sequential(*list(vgg.classifier._modules.values())[:-1])
+
+                def forward(self, x):
+                    x = x.flatten(start_dim=1)
+                    x = self.classifier(x)
+                    return x
+            box_head = BoxHead(vgg)
+            self.model = torchvision.models.detection.faster_rcnn.FasterRCNN(
+                backbone=backbone,
+                rpn_anchor_generator=anchor_generator,
+                box_roi_pool=roi_pooler,
+                box_head=box_head,
+                box_predictor=torchvision.models.detection.faster_rcnn.FastRCNNPredictor(4096, num_classes=3))
+
         elif self.nn_type == 'faster-rcnn-mobilenet':
             self.model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
             # get number of input features for the classifier
